ba9b6986acf15a9bd7478c56b1c61dc988e75998
HoangPhuc08022004 <lhphuc822004@gmail.com>
2024-11-12 08:31:21 +0700

    module3

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -3,25 +3,56 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const h1elment = React.createElement(
-  "h1",
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+const customerList=[
   {
-    id:"1",
-    className: "els1",
-    style:{
-      color:"red",
-      background:"yellow"
-
-    }
+    id:"01",
+    name:"phuc1",
+    class: "a1"
   },
-  "hello"
-)
+  {
+    id:"02",
+    name:"phuc2",
+    class: "a2"
+  },
+  {
+    id:"03",
+    name:"phuc3",
+    class: "a3"
+  }
+];
+const header=<>
+         <hr/>
+         <h3>Students</h3>
+</>
+const table=
+<table className='table table-striped'>
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Class</th>
+    </tr>
+  </thead>
+  <tbody>
+  {customerList.map((e,i)=>(
+      <tr>
+        <td>{e.id}</td>
+        <td>{e.name}</td>
+        <td>{e.class}</td>
+      </tr>
+    ))}
+  </tbody>
+</table> 
+const result=
+<div>
+  {header}
+  {table}
+</div>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  result
 );
 
 // If you want to start measuring performance in your app, pass a function
